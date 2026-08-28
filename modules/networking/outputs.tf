9c81062f55f4e454b7da7cfb6126696f7661e698
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-08-28 03:31:07 -0400

    fix: private subnet outputs

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -3,5 +3,17 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+  value = var.create_public_subnet ? aws_subnet.public[0].id : null
+}
+
+output "private_subnet_id" {
+  value = var.create_private_subnet ? aws_subnet.private[0].id : null
+}
+
+output "nat_gateway_id" {
+  value = (var.create_public_subnet && var.create_private_subnet) ? aws_nat_gateway.main[0].id : null
+}
+
+output "private_route_table_id" {
+  value = var.create_private_subnet ? aws_route_table.private[0].id : null
 }
